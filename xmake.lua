b412e59e603bb775d3bdfc20de8179060e85ada7
chen-qingyu <236521827@qq.com>
2024-01-26 06:32:58 +0800

    adjust xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -1,9 +1,9 @@
-add_rules("mode.debug", "mode.release")
 set_languages("c17")
+add_rules("mode.debug", "mode.release")
 add_requires("catch2")
 
 target("tests")
     set_kind("binary")
+    add_packages("catch2")
     add_files("sources/**.c")
     add_files("tests/*.cpp")
-    add_packages("catch2")
