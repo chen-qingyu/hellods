2930a26fe4ab8af978341f08feed0c909fe05342
chen-qingyu <236521827@qq.com>
2024-03-29 19:16:07 +0800

    refine ArrayQueue using ArrayDeque

--- sources/Queue/ArrayQueue.hpp
+++ sources/Queue/ArrayQueue.hpp
@@ -23,51 +23,18 @@
 #ifndef ARRAYQUEUE_HPP
 #define ARRAYQUEUE_HPP
 
-#include "../common/Container.hpp"
-#include "../common/utility.hpp"
+#include "../Deque/ArrayDeque.hpp"
 
 namespace hellods
 {
 
 /// Queue implemented by array.
 template <typename T>
-class ArrayQueue : public common::Container
+class ArrayQueue
 {
 private:
-    // Index of front in ring buffer. data[front] is the first element, except size == 0.
-    int front_;
-
-    // Available capacity.
-    int capacity_;
-
-    // Pointer to ring buffer.
-    T* data_;
-
-    // Convert logic index to ring buffer physical index.
-    int access(int logic_index) const
-    {
-        return (front_ + logic_index) % capacity_;
-    }
-
-    // Expand capacity safely for ring buffer. Require size == capacity.
-    void expand_capacity()
-    {
-        capacity_ = (capacity_ < MAX_CAPACITY / 2) ? capacity_ * 2 : MAX_CAPACITY; // double the capacity until MAX_CAPACITY
-        T* new_data = new T[capacity_];
-        int j = 0;
-        for (int i = front_; i < size_; ++i)
-        {
-            new_data[j++] = data_[i];
-        }
-        for (int i = 0; i < front_; ++i)
-        {
-            new_data[j++] = data_[i];
-        }
-
-        delete[] data_;
-        data_ = new_data;
-        front_ = 0;
-    }
+    // An array deque.
+    ArrayDeque<T> adeque_;
 
 public:
     /*
@@ -76,21 +43,14 @@ public:
 
     /// Create an empty queue.
     ArrayQueue()
-        : common::Container(0)
-        , front_(0)
-        , capacity_(INIT_CAPACITY)
-        , data_(new T[capacity_])
+        : adeque_()
     {
     }
 
     /// Create a queue based on the given initializer list.
     ArrayQueue(const std::initializer_list<T>& il)
-        : common::Container(int(il.size()))
-        , front_(0)
-        , capacity_(size_ > INIT_CAPACITY ? size_ : INIT_CAPACITY)
-        , data_(new T[capacity_])
+        : adeque_(il)
     {
-        std::copy(il.begin(), il.end(), data_);
     }
 
     /*
@@ -100,20 +60,7 @@ public:
     /// Check whether two queues are equal.
     bool operator==(const ArrayQueue& that) const
     {
-        if (size_ != that.size_)
-        {
-            return false;
-        }
-
-        for (int i = access(0), j = that.access(0), cnt = 0; cnt < size_; i = access(i + 1), j = that.access(j + 1), ++cnt)
-        {
-            if (data_[i] != that.data_[j])
-            {
-                return false;
-            }
-        }
-
-        return true;
+        return adeque_ == that.adeque_;
     }
 
     /// Check whether two queues are not equal.
@@ -123,21 +70,35 @@ public:
     }
 
     /*
+     * Examination
+     */
+
+    /// Get the number of elements of the queue.
+    int size() const
+    {
+        return adeque_.size();
+    }
+
+    /// Check if the queue is empty.
+    bool is_empty() const
+    {
+        return adeque_.is_empty();
+    }
+
+    /*
      * Access
      */
 
     /// Return the reference to the element at the front in the queue.
     T& front()
     {
-        common::check_empty(size_);
-        return data_[front_];
+        return adeque_.front();
     }
 
     /// Return the const reference to the element at the front in the queue.
     const T& front() const
     {
-        common::check_empty(size_);
-        return data_[front_];
+        return adeque_.front();
     }
 
     /*
@@ -147,36 +108,19 @@ public:
     /// Enqueue, insert an element at the rear of the queue.
     void enqueue(const T& element)
     {
-        common::check_full(size_, MAX_CAPACITY);
-
-        if (size_ == capacity_)
-        {
-            expand_capacity();
-        }
-
-        data_[(front_ + size_) % capacity_] = element;
-        size_++;
+        adeque_.push_back(element);
     }
 
     /// Dequeue, pop the front element of the queue.
     T dequeue()
     {
-        common::check_empty(size_);
-
-        T data = std::move(data_[front_]);
-        front_ = (front_ + 1) % capacity_;
-        size_--;
-
-        return data;
+        return adeque_.pop_front();
     }
 
     /// Remove all of the elements from the queue.
     ArrayQueue& clear()
     {
-        // If the elements themselves are pointers, the pointed-to memory is not touched in any way.
-        // Managing the pointer is the user's responsibility.
-        size_ = 0;
-        front_ = 0;
+        adeque_.clear();
 
         return *this;
     }
