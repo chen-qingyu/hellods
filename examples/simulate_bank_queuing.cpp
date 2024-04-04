a0f41156d7178eb4de073509495448918c2dfb33
chen-qingyu <236521827@qq.com>
2024-04-04 18:35:53 +0800

    🦄 refactor(examples): refine code

--- examples/simulate_bank_queuing.cpp
+++ examples/simulate_bank_queuing.cpp
@@ -40,8 +40,8 @@ std::ostream& operator<<(std::ostream& os, const Customer& c)
     return os << "time: " << c.time;
 }
 
-// 为新到顾客确定最佳队列
-int best_window(ArrayQueue<Customer> windows[], int win_num)
+// 为新到顾客确定最短队列
+static int best_window(ArrayQueue<Customer> windows[], int win_num)
 {
     int min_size = windows[0].size(), opt_win = 0;
     for (int i = 1; i < win_num; i++) // 在所有窗口中
@@ -58,23 +58,23 @@ int best_window(ArrayQueue<Customer> windows[], int win_num)
 // 按指定窗口数、服务总时间模拟银行业务
 void simulate_bank_queuing(int win_num, int serv_time)
 {
-    ArrayQueue<Customer>* windows = new ArrayQueue<Customer>[win_num]; // 为每一窗口创建一个队列
-
     std::srand((unsigned int)std::time(nullptr));
-    for (int now = 1; now <= serv_time; now++) // 每隔一个单位时间
+
+    auto windows = new ArrayQueue<Customer>[win_num]; // 为每一窗口创建一个队列
+    for (int now = 1; now <= serv_time; now++)        // 每隔一个单位时间
     {
-        std::cout << "Time: " << now << std::endl;
+        std::cout << "Time: " << now << "\n";
         if (rand() % (1 + win_num)) // 新顾客以一定的概率到达，概率正比于窗口数量
         {
             Customer c{1 + rand() % MAX_TIME};       // 新顾客到达，服务时长随机确定
             int win = best_window(windows, win_num); // 找出最佳（最短）的服务窗口
             windows[win].enqueue(c);                 // 新顾客加入对应的队列
-            std::cout << "New customer: " << c << " at Window " << win << std::endl;
+            std::cout << "New customer: " << c << " at Window " << win << "\n";
         }
 
         for (int i = 0; i < win_num; i++) // 分别检查
         {
-            std::cout << "Window " << i << ": " << windows[i] << std::endl;
+            std::cout << "Window " << i << ": " << windows[i] << "\n";
             if (!windows[i].is_empty()) // 各非空队列
             {
                 if (--windows[i].front().time <= 0) // 队首顾客的服务时长减少一个单位
@@ -83,7 +83,7 @@ void simulate_bank_queuing(int win_num, int serv_time)
                 }
             }
         }
-        std::cout << std::endl;
+        std::cout << "\n";
     }
 
     delete[] windows;
