3798afd32172d87799d99f8b19657d2afd460a69
chen-qingyu <236521827@qq.com>
2024-04-08 11:04:42 +0800

    ✨ feat(BinarySearchTree.hpp): add BinarySearchTree

--- sources/Tree/BinarySearchTree.hpp
+++ sources/Tree/BinarySearchTree.hpp
@@ -23,123 +23,365 @@
 #ifndef BINARYSEARCHTREE_HPP
 #define BINARYSEARCHTREE_HPP
 
-#include <stdbool.h> // bool
+#include "../common/Container.hpp"
+#include "../common/utility.hpp"
 
-/// A value of BinarySearchTreeItem that indicates not found.
-#define BINARY_SEARCH_TREE_NOT_FOUND (-1)
+#include <set> // for operator==()
 
-/// Traverse Option.
-enum TraverseOption
+#include "../Queue/ArrayQueue.hpp" // for traverse()
+
+namespace hellods
 {
-    PRE_ORDER,
-    IN_ORDER,
-    POST_ORDER,
-    LEVEL_ORDER
+
+/// Binary search tree.
+template <typename T>
+class BinarySearchTree : public common::Container
+{
+private:
+    // Binary search tree node.
+    struct Node
+    {
+        // Data stored in the node.
+        T data_;
+
+        // Pointer to the left child.
+        Node* left_;
+
+        // Pointer to the right child.
+        Node* right_;
+
+        // Create a node with given element.
+        Node(const T& data, Node* left = nullptr, Node* right = nullptr)
+            : data_(data)
+            , left_(left)
+            , right_(right)
+        {
+        }
+    };
+
+    // Pointer to the root.
+    Node* root_;
+
+    // Destroy the subtree rooted at that node recursively.
+    void destroy(Node* node)
+    {
+        if (node)
+        {
+            destroy(node->left_);
+            destroy(node->right_);
+            delete node;
+        }
+    }
+
+    enum TraverseOption;
+
+    // Traverse the tree in specified order.
+    template <typename F>
+    void traverse_node(Node* node, TraverseOption order, const F& p_trav) const
+    {
+        if (node)
+        {
+            switch (order)
+            {
+                case PreOrder:
+                {
+                    p_trav(node->data_);
+                    traverse_node(node->left_, PreOrder, p_trav);
+                    traverse_node(node->right_, PreOrder, p_trav);
+                    break;
+                }
+
+                case InOrder:
+                {
+                    traverse_node(node->left_, InOrder, p_trav);
+                    p_trav(node->data_);
+                    traverse_node(node->right_, InOrder, p_trav);
+                    break;
+                }
+
+                case PostOrder:
+                {
+                    traverse_node(node->left_, PostOrder, p_trav);
+                    traverse_node(node->right_, PostOrder, p_trav);
+                    p_trav(node->data_);
+                    break;
+                }
+
+                case LevelOrder:
+                {
+                    ArrayQueue<Node*> queue;
+                    queue.enqueue(node);
+                    while (!queue.is_empty())
+                    {
+                        node = queue.dequeue();
+                        p_trav(node->data_);
+                        if (node->left_)
+                        {
+                            queue.enqueue(node->left_);
+                        }
+                        if (node->right_)
+                        {
+                            queue.enqueue(node->right_);
+                        }
+                    }
+                    break;
+                }
+
+                default:
+                {
+                    throw std::runtime_error("Error: Invalid order for traverse.");
+                }
+            }
+        }
+    }
+
+    // Insert node recursively.
+    Node* insert_node(Node* node, const T& element)
+    {
+        if (node == nullptr)
+        {
+            node = new Node(element);
+            size_++;
+        }
+        else
+        {
+            if (element < node->data_)
+            {
+                node->left_ = insert_node(node->left_, element);
+            }
+            else if (element > node->data_)
+            {
+                node->right_ = insert_node(node->right_, element);
+            }
+        }
+
+        return node;
+    }
+
+    // Find subtree minimum node.
+    Node* find_min(Node* node) const
+    {
+        while (node->left_) // node is not null
+        {
+            node = node->left_;
+        }
+        return node;
+    }
+
+    // Remove node recursively.
+    Node* remove_node(Node* node, const T& element)
+    {
+        if (node)
+        {
+            if (element < node->data_)
+            {
+                node->left_ = remove_node(node->left_, element);
+            }
+            else if (element > node->data_)
+            {
+                node->right_ = remove_node(node->right_, element);
+            }
+            else // element == node->data_
+            {
+                if (node->left_ && node->right_)
+                {
+                    Node* min = find_min(node->right_);
+                    node->data_ = min->data_;
+                    node->right_ = remove_node(node->right_, min->data_);
+                }
+                else
+                {
+                    Node* tmp = node;
+                    node = node->left_ ? node->left_ : node->right_;
+                    delete tmp;
+                    size_--;
+                }
+            }
+        }
+
+        return node;
+    }
+
+    // Returns the maximum depth of the subtree rooted at the node.
+    int depth_node(Node* node) const
+    {
+        return node == nullptr ? 0 : 1 + std::max(depth_node(node->left_), depth_node(node->right_));
+    }
+
+public:
+    /*
+     * Constructor / Destructor
+     */
+
+    /// Create an empty tree.
+    BinarySearchTree()
+        : common::Container(0)
+        , root_(nullptr)
+    {
+    }
+
+    /// Create a tree based on the given initializer list.
+    BinarySearchTree(const std::initializer_list<T>& il)
+        : BinarySearchTree()
+    {
+        for (auto it = il.begin(); it != il.end(); ++it)
+        {
+            insert(*it);
+        }
+    }
+
+    /// Destroy the tree object.
+    ~BinarySearchTree()
+    {
+        destroy(root_);
+    }
+
+    /*
+     * Comparison
+     */
+
+    /// Check whether two trees are equal.
+    bool operator==(const BinarySearchTree& that) const
+    {
+        if (size_ != that.size_)
+        {
+            return false;
+        }
+
+        // count elements in each tree
+        std::set<T> this_set, that_set;
+        traverse_node(root_, InOrder, [&](const T& e)
+                      { this_set.insert(e); });
+        traverse_node(that.root_, InOrder, [&](const T& e)
+                      { that_set.insert(e); });
+
+        return this_set == that_set;
+    }
+
+    /// Check whether two trees are not equal.
+    bool operator!=(const BinarySearchTree& that) const
+    {
+        return !(*this == that);
+    }
+
+    /*
+     * Access
+     */
+
+    /// Return the smallest element of the tree
+    T min() const
+    {
+        common::check_empty(size());
+        return find_min(root_)->data_;
+    }
+
+    /// Return the largest element of the tree
+    T max() const
+    {
+        common::check_empty(size());
+        Node* current = root_;
+        while (current->right_)
+        {
+            current = current->right_;
+        }
+        return current->data_;
+    }
+
+    /*
+     * Examination
+     */
+
+    /// Traverse Option.
+    enum TraverseOption
+    {
+        PreOrder,
+        InOrder,
+        PostOrder,
+        LevelOrder
+    };
+
+    /// Traverse the tree.
+    template <typename F>
+    void traverse(TraverseOption order, const F& p_trav) const
+    {
+        traverse_node(root_, order, p_trav);
+    }
+
+    /// Determine whether an element is in the tree.
+    bool contains(const T& element) const
+    {
+        Node* current = root_;
+        while (current)
+        {
+            if (element > current->data_)
+            {
+                current = current->right_;
+            }
+            else if (element < current->data_)
+            {
+                current = current->left_;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /// Find the maximum depth of the tree. Empty tree depth is 0.
+    int depth() const
+    {
+        return depth_node(root_);
+    }
+
+    /*
+     * Manipulation
+     */
+
+    /// Insert the specified element in the tree. Return whether the element was newly inserted.
+    bool insert(const T& element)
+    {
+        int old_size = size_;
+        root_ = insert_node(root_, element);
+        return old_size != size_;
+    }
+
+    /// Remove the specified element from the tree. Return whether such an element was present.
+    bool remove(const T& element)
+    {
+        int old_size = size_;
+        root_ = remove_node(root_, element);
+        return old_size != size_;
+    }
+
+    /// Remove all of the elements from the tree.
+    BinarySearchTree& clear()
+    {
+        if (size_ != 0)
+        {
+            size_ = 0;
+            destroy(root_);
+            root_ = nullptr;
+        }
+
+        return *this;
+    }
+
+    /*
+     * Print
+     */
+
+    /// Print the tree.
+    friend std::ostream& operator<<(std::ostream& os, const BinarySearchTree& tree)
+    {
+        os << "Tree(";
+        tree.traverse_node(tree.root_, LevelOrder, [&](const T& e)
+                           { os << (tree.root_->data_ == e ? "" : ", ") << e; });
+        return os << ")";
+    }
 };
 
-/// Binary Search Tree Item.
-typedef int BinarySearchTreeItem;
-
-/// Binary Search Tree.
-typedef struct BinarySearchTree BinarySearchTree;
-
-/**
- * @brief 创建一个空二叉搜索树
- *
- * @return 一个指向空二叉搜索树的指针
- */
-BinarySearchTree* BinarySearchTree_Create(void);
-
-/**
- * @brief 销毁一个二叉搜索树
- *
- * @param self 一个指向待销毁二叉搜索树的指针
- */
-void BinarySearchTree_Destroy(BinarySearchTree* self);
-
-/**
- * @brief 求二叉搜索树的元素个数
- *
- * @param self 一个指向二叉搜索树的指针
- * @return 二叉搜索树的元素个数
- */
-int BinarySearchTree_Size(const BinarySearchTree* self);
-
-/**
- * @brief 判断二叉搜索树是否为空
- *
- * @param self 一个指向二叉搜索树的指针
- * @return 如果二叉搜索树为空则返回 true ，否则返回 false
- */
-bool BinarySearchTree_IsEmpty(const BinarySearchTree* self);
-
-/**
- * @brief 遍历一个二叉搜索树
- *
- * @param self 一个指向二叉搜索树的指针
- * @param order 一个枚举遍历类型
- *              - PRE_ORDER   先序遍历
- *              - IN_ORDER    中序遍历
- *              - POST_ORDER  后序遍历
- *              - LEVEL_ORDER 层次遍历
- * @param p_trav 一个对遍历到的每个元素进行操作的函数的指针
- */
-void BinarySearchTree_Traverse(BinarySearchTree* self, enum TraverseOption order, void (*p_trav)(const BinarySearchTreeItem data));
-
-/**
- * @brief 在一个二叉搜索树中寻找元素 data
- *
- * @param self 一个指向二叉搜索树的指针
- * @param data 一个待寻找元素
- * @return 元素 data 或者 BINARY_SEARCH_TREE_NOT_FOUND
- */
-BinarySearchTreeItem BinarySearchTree_Find(const BinarySearchTree* self, BinarySearchTreeItem data);
-
-/**
- * @brief 在一个二叉搜索树中寻找最小元素
- *
- * @param self 一个指向二叉搜索树的指针
- * @return 最小的元素或者 BINARY_SEARCH_TREE_NOT_FOUND
- */
-BinarySearchTreeItem BinarySearchTree_FindMin(const BinarySearchTree* self);
-
-/**
- * @brief 在一个二叉搜索树中寻找最大元素
- *
- * @param self 一个指向二叉搜索树的指针
- * @return 最大的元素或者 BINARY_SEARCH_TREE_NOT_FOUND
- */
-BinarySearchTreeItem BinarySearchTree_FindMax(const BinarySearchTree* self);
-
-/**
- * @brief 在一个二叉搜索树中插入元素 data
- *
- * @param self 一个指向二叉搜索树的指针
- * @param data 一个待插入元素
- */
-void BinarySearchTree_Insert(BinarySearchTree* self, BinarySearchTreeItem data);
-
-/**
- * @brief 在一个二叉搜索树中删除元素 data
- *
- * @param self 一个指向二叉搜索树的指针
- * @param data 一个待删除元素
- */
-void BinarySearchTree_Remove(BinarySearchTree* self, BinarySearchTreeItem data);
-
-/**
- * @brief 求二叉搜索树的最大深度，空树深度为0
- *
- * @param self 一个指向二叉搜索树的指针
- * @return 二叉搜索树的最大深度
- */
-int BinarySearchTree_Depth(const BinarySearchTree* self);
-
-/**
- * @brief 清空二叉搜索树的内容
- *
- * @param self 一个指向二叉搜索树的指针
- */
-void BinarySearchTree_Clear(BinarySearchTree* self);
+} // namespace hellods
 
 #endif // BINARYSEARCHTREE_HPP
---
+++ tests/test_tree.cpp
@@ -0,0 +1,115 @@
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_exception.hpp>
+
+#include "../sources/Tree/BinarySearchTree.hpp"
+
+using Catch::Matchers::Message;
+
+using namespace hellods;
+
+template <typename Tree>
+void test()
+{
+    // Constructor / Destructor
+    Tree empty;
+    Tree some = {3, 1, 2, 5, 4};
+
+    // Comparison
+    REQUIRE(empty == Tree());
+    REQUIRE(some == Tree({5, 4, 3, 2, 1}));
+    REQUIRE(empty != some);
+    REQUIRE(some != Tree({1, 2, 3, 4, 6}));
+
+    REQUIRE(Tree({2, 3, 3, 3}) == Tree({2, 2, 3, 3}));
+    REQUIRE(Tree({2, 3, 3, 3}) != Tree({3, 3, 3, 3}));
+
+    // Access
+    REQUIRE(some.min() == 1);
+    REQUIRE(some.max() == 5);
+    REQUIRE_THROWS_MATCHES(empty.min(), std::runtime_error, Message("Error: The container is empty."));
+    REQUIRE_THROWS_MATCHES(empty.max(), std::runtime_error, Message("Error: The container is empty."));
+
+    // Examination
+    REQUIRE(empty.size() == 0);
+    REQUIRE(empty.is_empty() == true);
+
+    REQUIRE(some.size() == 5);
+    REQUIRE(some.is_empty() == false);
+
+    std::ostringstream buf;
+
+    some.traverse(Tree::PreOrder, [&](const auto& e)
+                  { buf << e << " "; });
+    REQUIRE(buf.str() == "3 1 2 5 4 ");
+    buf.str("");
+
+    some.traverse(Tree::InOrder, [&](const auto& e)
+                  { buf << e << " "; });
+    REQUIRE(buf.str() == "1 2 3 4 5 ");
+    buf.str("");
+
+    some.traverse(Tree::PostOrder, [&](const auto& e)
+                  { buf << e << " "; });
+    REQUIRE(buf.str() == "2 1 4 5 3 ");
+    buf.str("");
+
+    some.traverse(Tree::LevelOrder, [&](const auto& e)
+                  { buf << e << " "; });
+    REQUIRE(buf.str() == "3 1 5 2 4 ");
+    buf.str("");
+
+    REQUIRE(some.contains(1) == true);
+    REQUIRE(some.contains(0) == false);
+    REQUIRE(empty.contains(1) == false);
+    REQUIRE(empty.contains(0) == false);
+
+    REQUIRE(some.depth() == 3);
+    REQUIRE(empty.depth() == 0);
+
+    // Manipulation
+    REQUIRE(empty.insert(1) == true);
+    REQUIRE(empty == Tree({1}));
+    REQUIRE(empty.insert(0) == true);
+    REQUIRE(empty == Tree({0, 1}));
+    REQUIRE(empty.insert(2) == true);
+    REQUIRE(empty == Tree({0, 1, 2}));
+    REQUIRE(empty.insert(3) == true);
+    REQUIRE(empty == Tree({0, 1, 2, 3}));
+    REQUIRE(empty.insert(3) == false);
+    REQUIRE(empty == Tree({0, 1, 2, 3}));
+
+    REQUIRE(empty.remove(3) == true);
+    REQUIRE(empty == Tree({0, 1, 2}));
+    REQUIRE(empty.remove(2) == true);
+    REQUIRE(empty == Tree({0, 1}));
+    REQUIRE(empty.remove(1) == true);
+    REQUIRE(empty == Tree({0}));
+    REQUIRE(empty.remove(0) == true);
+    REQUIRE(empty == Tree({}));
+    REQUIRE(empty.remove(0) == false);
+    REQUIRE(empty == Tree({}));
+
+    REQUIRE(some.clear() == empty);
+    REQUIRE(some.clear() == empty); // double clear
+
+    // Print
+    std::ostringstream oss;
+
+    oss << Tree({});
+    REQUIRE(oss.str() == "Tree()");
+    oss.str("");
+
+    oss << Tree({1});
+    REQUIRE(oss.str() == "Tree(1)");
+    oss.str("");
+
+    oss << Tree({1, 2, 3, 4, 5});
+    REQUIRE(oss.str() == "Tree(1, 2, 3, 4, 5)");
+    oss.str("");
+}
+
+TEST_CASE("BinarySearchTree")
+{
+    test<BinarySearchTree<int>>();
+    test<BinarySearchTree<double>>();
+}
