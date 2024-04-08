29df397d82b0460055259bf4ff6af1425abc1cb2
chen-qingyu <236521827@qq.com>
2024-04-08 11:03:56 +0800

    🦄 refactor(test_deque.cpp): refine includes

--- tests/test_deque.cpp
+++ tests/test_deque.cpp
@@ -1,8 +1,6 @@
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/matchers/catch_matchers_exception.hpp>
 
-#include <sstream>
-
 #include "../sources/Deque/ArrayDeque.hpp"
 #include "../sources/Deque/LinkedDeque.hpp"
 
