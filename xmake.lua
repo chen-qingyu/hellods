4148b6573c737e388c77cd161692e624fa93f9bc
chen-qingyu <236521827@qq.com>
2024-05-27 00:04:52 +0800

    🐳 chore(xmake.lua): update info and target

--- xmake.lua
+++ xmake.lua
@@ -1,12 +1,15 @@
+set_project("hellods")
+set_version("2.4.0")
 set_languages("cxx17")
+
 add_rules("mode.debug", "mode.release")
 add_requires("catch2")
 
-target("tests")
+target("test")
     set_kind("binary")
     add_packages("catch2")
     add_files("tests/*.cpp")
 
-target("examples")
+target("example")
     set_kind("binary")
     add_files("examples/*.cpp")
