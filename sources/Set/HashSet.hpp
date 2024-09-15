dae6e7a6d335373b787eb488b2fe0e91bfbb4f7f
chen-qingyu <236521827@qq.com>
2024-09-15 18:11:12 +0800

    add file for module Set

---
+++ sources/Set/HashSet.hpp
@@ -0,0 +1,13 @@
+/**
+ * @file HashSet.hpp
+ * @author Chen QingYu <chen_qingyu@qq.com>
+ * @brief Hash set.
+ * @date 2024.09.15
+ */
+
+#ifndef HASHSET_HPP
+#define HASHSET_HPP
+
+// TODO use HashMap<K, ()> like Rust.
+
+#endif // HASHSET_HPP
