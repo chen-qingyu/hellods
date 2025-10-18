f204769ac0026d7209d228845f0691babf95ec2a
chen-qingyu <chen_qingyu@qq.com>
2025-10-18 18:29:48 +0800

    add HashSet!

--- sources/Set/HashSet.hpp
+++ sources/Set/HashSet.hpp
@@ -8,6 +8,215 @@
 #ifndef HASHSET_HPP
 #define HASHSET_HPP
 
-// TODO use HashMap<K, ()> like Rust.
+#include "../Map/HashMap.hpp"
+
+namespace hellods
+{
+
+/// Hash set.
+template <typename T, typename Hash = std::hash<T>, typename Eq = std::equal_to<T>>
+class HashSet : public detail::Container
+{
+protected:
+    // Dummy value type for internal map.
+    struct Dummy
+    {
+    };
+
+    // Internal map that stores actual data.
+    HashMap<T, Dummy, Hash, Eq> map_;
+
+public:
+    /// Set iterator class.
+    ///
+    /// Walk the set in random order.
+    ///
+    /// Because the internal items of the set have a fixed position,
+    /// thus the iterator of the set does not support modification for item.
+    class Iterator
+    {
+        friend class HashSet;
+
+    protected:
+        // Internal map iterator.
+        HashMap<T, Dummy, Hash, Eq>::Iterator map_it_;
+
+        // Constructor.
+        Iterator(HashMap<T, Dummy, Hash, Eq>::Iterator it)
+            : map_it_(it)
+        {
+        }
+
+    public:
+        using iterator_category = std::input_iterator_tag;
+        using value_type = T;
+        using difference_type = int;
+        using pointer = value_type*;
+        using reference = value_type&;
+
+        bool operator==(const Iterator& that) const
+        {
+            return map_it_ == that.map_it_;
+        }
+
+        const T& operator*() const
+        {
+            return map_it_->first;
+        }
+
+        const T* operator->() const
+        {
+            return &(map_it_->first);
+        }
+
+        Iterator& operator++()
+        {
+            ++map_it_;
+            return *this;
+        }
+
+        Iterator operator++(int)
+        {
+            auto it = *this;
+            ++*this;
+            return it;
+        }
+
+        Iterator& operator--()
+        {
+            --map_it_;
+            return *this;
+        }
+
+        Iterator operator--(int)
+        {
+            auto it = *this;
+            --*this;
+            return it;
+        }
+    };
+
+public:
+    /*
+     * Constructor / Destructor
+     */
+
+    /// Create an empty set.
+    HashSet()
+        : detail::Container(0)
+        , map_()
+    {
+    }
+
+    /// Create a set based on the given initializer list.
+    HashSet(const std::initializer_list<T>& il)
+        : HashSet()
+    {
+        for (auto it = il.begin(); it != il.end(); ++it)
+        {
+            insert(*it);
+        }
+    }
+
+    /*
+     * Comparison
+     */
+
+    /// Check whether two sets are equal.
+    bool operator==(const HashSet& that) const
+    {
+        if (size_ != that.size_)
+        {
+            return false;
+        }
+
+        for (const auto& item : that)
+        {
+            if (!contains(item))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /*
+     * Iterator
+     */
+
+    /// Return an iterator to the first element of the set.
+    Iterator begin() const
+    {
+        return Iterator(map_.begin());
+    }
+
+    /// Return an iterator to the element following the last element of the set.
+    Iterator end() const
+    {
+        return Iterator(map_.end());
+    }
+
+    /*
+     * Examination
+     */
+
+    /// Return an iterator to the first occurrence of the specified item, or end() if the set does not contains the item.
+    Iterator find(const T& item) const
+    {
+        return Iterator(map_.find(item));
+    }
+
+    /// Determine whether a item is in the set.
+    bool contains(const T& item) const
+    {
+        return map_.contains(item);
+    }
+
+    /*
+     * Manipulation
+     */
+
+    /// Insert a new item into the set. Return whether the item was newly inserted.
+    bool insert(const T& item)
+    {
+        bool inserted = map_.insert(item, Dummy());
+        if (inserted)
+        {
+            size_++;
+        }
+        return inserted;
+    }
+
+    /// Remove the item corresponding to the item in the set. Return whether such a item was present.
+    bool remove(const T& item)
+    {
+        bool removed = map_.remove(item);
+        if (removed)
+        {
+            size_--;
+        }
+        return removed;
+    }
+
+    /// Remove all of the elements from the set.
+    void clear()
+    {
+        map_.clear();
+        size_ = 0;
+    }
+
+    /*
+     * Print
+     */
+
+    /// Print the set.
+    friend std::ostream& operator<<(std::ostream& os, const HashSet& set)
+    {
+        return detail::print(os, set.begin(), set.end(), "Set");
+    }
+};
+
+} // namespace hellods
 
 #endif // HASHSET_HPP
---
+++ tests/test_set.cpp
@@ -0,0 +1,140 @@
+#include "tool.hpp"
+
+#include "../sources/Set/HashSet.hpp"
+
+using namespace hellods;
+
+template <typename Set>
+void test()
+{
+    // Constructor / Destructor
+    Set empty;
+    Set some = {1, 2, 3};
+
+    // Comparison
+    REQUIRE(empty == Set());
+    REQUIRE(some == Set({1, 2, 3}));
+    REQUIRE(empty != some);
+    REQUIRE(some != Set({1, 2, 4}));
+    REQUIRE(some != Set({1, 2}));
+
+    // Iterator
+    REQUIRE(empty.begin() == empty.end());
+
+    int sum = 0;
+    for (auto it = some.begin(); it != some.end(); ++it)
+    {
+        sum += *it;
+    }
+    REQUIRE(sum == 6);
+
+    sum = 0;
+    for (const auto& e : some)
+    {
+        sum += e;
+    }
+    REQUIRE(sum == 6);
+
+    auto it = some.begin();
+    REQUIRE(*it == 1);
+    REQUIRE(*++it == 2);
+    REQUIRE(*++it == 3);
+    REQUIRE(++it == some.end());
+    REQUIRE(*--it == 3);
+    REQUIRE(*--it == 2);
+    REQUIRE(*--it == 1);
+    REQUIRE(it == some.begin());
+
+    // Examination
+    REQUIRE(empty.size() == 0);
+    REQUIRE(some.size() == 3);
+
+    REQUIRE(empty.is_empty() == true);
+    REQUIRE(some.is_empty() == false);
+
+    REQUIRE(empty.find(1) == empty.end());
+    REQUIRE(*some.find(2) == 2);
+    REQUIRE(some.find(4) == some.end());
+
+    REQUIRE(empty.contains(1) == false);
+    REQUIRE(empty.contains(0) == false);
+    REQUIRE(some.contains(1) == true);
+    REQUIRE(some.contains(0) == false);
+
+    // Manipulation
+    REQUIRE(empty.insert(1) == true);
+    REQUIRE(empty == Set({1}));
+    REQUIRE(empty.insert(2) == true);
+    REQUIRE(empty == Set({1, 2}));
+    REQUIRE(empty.insert(3) == true);
+    REQUIRE(empty == Set({1, 2, 3}));
+    REQUIRE(empty.insert(3) == false);
+    REQUIRE(empty == Set({1, 2, 3}));
+
+    REQUIRE(empty.remove(3) == true);
+    REQUIRE(empty == Set({1, 2}));
+    REQUIRE(empty.remove(2) == true);
+    REQUIRE(empty == Set({1}));
+    REQUIRE(empty.remove(1) == true);
+    REQUIRE(empty == Set({}));
+    REQUIRE(empty.remove(1) == false);
+    REQUIRE(empty == Set({}));
+
+    some.clear();
+    REQUIRE(some == empty);
+    some.clear(); // double clear
+    REQUIRE(some == empty);
+
+    // Test expansion
+    Set large;
+    for (int i = 0; i < 100; i++)
+    {
+        REQUIRE(large.insert(i) == true);
+    }
+    REQUIRE(large.size() == 100);
+    for (int i = 0; i < 100; i++)
+    {
+        REQUIRE(large.contains(i) == true);
+    }
+    for (int i = 0; i < 100; i++)
+    {
+        REQUIRE(large.remove(i) == true);
+    }
+    REQUIRE(large.size() == 0);
+    REQUIRE(large.is_empty() == true);
+
+    // Print
+    std::ostringstream oss;
+
+    oss << Set({});
+    REQUIRE(oss.str() == "Set()");
+    oss.str("");
+
+    oss << Set({1});
+    REQUIRE(oss.str() == "Set(1)");
+    oss.str("");
+
+    oss << Set({1, 2, 3});
+    REQUIRE(oss.str() == "Set(1, 2, 3)");
+    oss.str("");
+}
+
+template <>
+struct std::hash<EqType> // explicit specialization
+{
+    std::size_t operator()(const EqType&) const
+    {
+        static std::size_t id = 0;
+        return id++;
+    }
+};
+
+TEST_CASE("HashSet")
+{
+    test<HashSet<int>>();
+
+    HashSet<EqType> empty;
+    HashSet<EqType> some = {EqType(), EqType(), EqType()};
+    REQUIRE(empty.size() == 0);
+    REQUIRE(some.size() == 3);
+}
